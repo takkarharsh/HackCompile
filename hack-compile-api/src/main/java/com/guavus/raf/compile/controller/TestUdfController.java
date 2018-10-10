35c2dda313930b8ffa51a9029a5276c91f557b5d
Priyansh-Guavus <priyansh.jain@guavus.com>
2018-10-10 16:35:07 +0530

    crossorigin

--- hack-compile-api/src/main/java/com/guavus/raf/compile/controller/TestUdfController.java
+++ hack-compile-api/src/main/java/com/guavus/raf/compile/controller/TestUdfController.java
@@ -8,16 +8,14 @@ import com.guavus.raf.compile.service.CompileTestService;
 import com.guavus.raf.compile.utils.ErrorCodes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import scala.Tuple2;
 
 import java.util.List;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/hack/test")
 public class TestUdfController {
 
