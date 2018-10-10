56818c137ac74dd19d7c0be1ad37a242ef895e0c
harsh.takkar <harsh.takkar@guavus.com>
2018-10-10 11:38:00 +0530

    swatkats

--- hack-compile-api/src/main/java/com/guavus/raf/compile/controller/UdfTemplateController.java
+++ hack-compile-api/src/main/java/com/guavus/raf/compile/controller/UdfTemplateController.java
@@ -1,9 +1,11 @@
 package com.guavus.raf.compile.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/hack/template")
 public class UdfTemplateController {
 
